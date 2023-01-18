b304313b4d90cd7cbda2c5089d5aba391df11ecd
egota1n <57810535+egota1n@users.noreply.github.com>
2023-01-19 02:43:04 +0500

    Fix

--- coaching/assets/js/script.js
+++ coaching/assets/js/script.js
@@ -3,6 +3,16 @@ let x2 = document.querySelector('.program-block2').classList
 let x3 = document.querySelector('.program-block3').classList
 let x4 = document.querySelector('.program-block4').classList
 let x5 = document.querySelector('.program-block5').classList
+let x6 = document.querySelector('.program-block6').classList
+let x7 = document.querySelector('.program-block7').classList
+let x8 = document.querySelector('.program-block8').classList
+let x9 = document.querySelector('.program-block9').classList
+let x10 = document.querySelector('.program-block10').classList
+let x11 = document.querySelector('.program-block11').classList
+let x12 = document.querySelector('.program-block12').classList
+let x13 = document.querySelector('.program-block13').classList
+let x14 = document.querySelector('.program-block14').classList
+let x15 = document.querySelector('.program-block15').classList
 
 document.getElementById('input-program1').onclick = () => {
     x1.remove("active");
@@ -49,23 +59,142 @@ document.getElementById('input-program5').onclick = () => {
     x5.add("active");
 };
 
+document.getElementById('input-program6').onclick = () => {
+	x6.remove("active");
+    x7.remove("active");
+    x8.remove("active");
+    x9.remove("active");
+    x10.remove("active");
+    x6.add("active");
+};
 
-// document.getElementById('program__link-block1').onclick = () => {
-//     document.getElementById('input-program1').checked = true;
-// };
+document.getElementById('input-program7').onclick = () => {
+	x6.remove("active");
+    x7.remove("active");
+    x8.remove("active");
+    x9.remove("active");
+    x10.remove("active");
+    x7.add("active");
+};
 
-// document.getElementById('program__link-block2').onclick = () => {
-//     document.getElementById('input-program2').checked = true;
-// };
+document.getElementById('input-program8').onclick = () => {
+	x6.remove("active");
+    x7.remove("active");
+    x8.remove("active");
+    x9.remove("active");
+    x10.remove("active");
+    x8.add("active");
+};
 
-// document.getElementById('program__link-block3').onclick = () => {
-//     document.getElementById('input-program3').checked = true;
-// };
+document.getElementById('input-program9').onclick = () => {
+	x6.remove("active");
+    x7.remove("active");
+    x8.remove("active");
+    x9.remove("active");
+    x10.remove("active");
+    x9.add("active");
+};
 
-// document.getElementById('program__link-block4').onclick = () => {
-//     document.getElementById('input-program4').checked = true;
-// };
+document.getElementById('input-program10').onclick = () => {
+	x6.remove("active");
+    x7.remove("active");
+    x8.remove("active");
+    x9.remove("active");
+    x10.remove("active");
+    x10.add("active");
+};
 
-// document.getElementById('program__link-block5').onclick = () => {
-//     document.getElementById('input-program5').checked = true;
-// };
+document.getElementById('input-program11').onclick = () => {
+	x11.remove("active");
+    x12.remove("active");
+    x13.remove("active");
+    x14.remove("active");
+    x15.remove("active");
+    x11.add("active");
+};
+
+document.getElementById('input-program12').onclick = () => {
+	x11.remove("active");
+    x12.remove("active");
+    x13.remove("active");
+    x14.remove("active");
+    x15.remove("active");
+    x12.add("active");
+};
+
+document.getElementById('input-program13').onclick = () => {
+	x11.remove("active");
+    x12.remove("active");
+    x13.remove("active");
+    x14.remove("active");
+    x15.remove("active");
+    x13.add("active");
+};
+
+document.getElementById('input-program14').onclick = () => {
+	x11.remove("active");
+    x12.remove("active");
+    x13.remove("active");
+    x14.remove("active");
+    x15.remove("active");
+    x14.add("active");
+};
+
+document.getElementById('input-program15').onclick = () => {
+	x11.remove("active");
+    x12.remove("active");
+    x13.remove("active");
+    x14.remove("active");
+    x15.remove("active");
+    x15.add("active");
+};
+
+
+document.querySelector('.program-block5 .content__about-block-arrow-next').onclick = () => {
+    document.querySelector('.program1').classList.remove("active");
+    document.querySelector('.program3').classList.remove("active");
+    document.querySelector('.program2').classList.add("active");
+};
+
+document.querySelector('.program-block6 .content__about-block-arrow-prev').onclick = () => {
+    document.querySelector('.program2').classList.remove("active");
+    document.querySelector('.program3').classList.remove("active");
+    document.querySelector('.program1').classList.add("active");
+};
+
+document.querySelector('.program-block10 .content__about-block-arrow-next').onclick = () => {
+    document.querySelector('.program1').classList.remove("active");
+    document.querySelector('.program2').classList.remove("active");
+    document.querySelector('.program3').classList.add("active");
+};
+
+document.querySelector('.program-block11 .content__about-block-arrow-prev').onclick = () => {
+    document.querySelector('.program1').classList.remove("active");
+    document.querySelector('.program3').classList.remove("active");
+    document.querySelector('.program2').classList.add("active");
+};
+
+
+document.querySelector('.program1 .program-switch-next').onclick = () => {
+    document.querySelector('.program1').classList.remove("active");
+    document.querySelector('.program3').classList.remove("active");
+    document.querySelector('.program2').classList.add("active");
+};
+
+document.querySelector('.program2 .program-switch-prev').onclick = () => {
+    document.querySelector('.program2').classList.remove("active");
+    document.querySelector('.program3').classList.remove("active");
+    document.querySelector('.program1').classList.add("active");
+};
+
+document.querySelector('.program2 .program-switch-next').onclick = () => {
+    document.querySelector('.program1').classList.remove("active");
+    document.querySelector('.program2').classList.remove("active");
+    document.querySelector('.program3').classList.add("active");
+};
+
+document.querySelector('.program3 .program-switch-prev').onclick = () => {
+    document.querySelector('.program1').classList.remove("active");
+    document.querySelector('.program3').classList.remove("active");
+    document.querySelector('.program2').classList.add("active");
+};
